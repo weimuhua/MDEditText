0e0aea82ce0b110f9fada57d5499ca2e4d437417
weimuhua <weimuhua0812@126.com>
2016-11-08 14:24:40 +0800

    change : custom MDEditText underline programmly.

--- app/src/main/java/com/wayne/mdedittext/MainActivity.java
+++ app/src/main/java/com/wayne/mdedittext/MainActivity.java
@@ -1,7 +1,10 @@
 package com.wayne.mdedittext;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+
+import com.wayne.mdedittext.ui.MDEditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    @SuppressWarnings("deprecation")
+    private void initView() {
+        MDEditText customUnderlineEt1 = (MDEditText) findViewById(R.id.custom_underline_et1);
+        customUnderlineEt1.getEditText().setBackgroundDrawable(
+                ContextCompat.getDrawable(this, R.drawable.md_edittext_bkg));
+
+        MDEditText customUnderlineEt2 = (MDEditText) findViewById(R.id.custom_underline_et2);
+        customUnderlineEt2.getEditText().setBackgroundDrawable(
+                ContextCompat.getDrawable(this, R.drawable.md_edittext_bkg));
     }
 }
